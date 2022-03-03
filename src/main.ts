019e1e34de5e7ea9d757d6f6f6f8f42a850b2f1f
codera21 <atul.nepal@gmail.com>
2022-03-04 02:31:42 +0545

    basic url done

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,63 @@
+import fetch from "node-fetch";
+import cheerio from "cheerio";
+import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2))
+console.log(args);
+
+// input url: this should come from cli 
+// const input_url = 'https://www.sastodeal.com/fumo-classic-faux-leather-bean-bag-xxxl-black-with-beans-fumo-sd-gg-002.html';
+
+const inputUrl: string = args['url'];
+
+if (!inputUrl) {
+    console.log("Please enter url")
+    process.exit();
+}
+
+const res = await fetch(inputUrl);
+// console.log(await res.text());
+const $ = cheerio.load(await res.text());
+
+const meta = $('meta');
+
+const output = {};
+for (let i: number = 0; i < meta.length; i++) {
+    try {
+
+        const val = meta.eq(i).attr('content');
+        let key = meta.eq(i).attr('name');
+        if (key === undefined) {
+            key = meta.eq(i).attr('property');
+        }
+
+        if (key) {
+            output[key] = val;
+        }
+
+
+
+    } catch (err) {
+        console.log('content or name not out in meta tag');
+    }
+}
+
+console.log(output);
+
+
+
+
+// fetch(input_url).then(async res => {
+//     const $ = cheerio.load(await res.text());
+// }).catch(() => {
+//     console.log("ooops !!");
+// });
+
+
+
+
+
+
+
 
 
