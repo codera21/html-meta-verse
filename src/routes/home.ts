09a83a3ec82cb0a60fdff9978ca888a1140474b4
codera21 <atul.nepal@gmail.com>
2022-03-06 10:15:50 +0545

    pass url as query string

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -2,9 +2,8 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 import metaTags from '../lib/metaTags';
 
 export default async function routes(fastify, _options) {
-  fastify.get('/', async (_request: FastifyRequest, _reply: FastifyReply) => {
-    let url: string =
-      'https://www.sastodeal.com/fumo-classic-faux-leather-bean-bag-xxxl-black-with-beans-fumo-sd-gg-002.html';
+  fastify.get('/', async (request: FastifyRequest, _reply: FastifyReply) => {
+    let url: string = request.query['url'];
 
     return await metaTags.getMetaTags(url);
   });
